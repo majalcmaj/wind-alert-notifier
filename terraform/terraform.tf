033478cb416308ab2a0893aa4b7bea111dc3876c
Michał Ciesielski <majalcmaj@gmail.com>
2026-06-07 22:38:13 +0200

    Use terraform

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -7,5 +7,13 @@ terraform {
   }
 
   required_version = ">= 1.13"
+
+  backend "s3" {
+    bucket       = "wind-alert-terraform-state-513532022998"
+    key          = "wind-alert/terraform.tfstate"
+    region       = "eu-central-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
 
